c4f293e6209cbe3c03fa1b6b134fc66b41976c90
Raphael <lebarbi232@etu.unicaen.fr>
2024-09-24 10:44:46 +0200

    fuctional working/pause alternation

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-let seconds = 10;
-let minutes = 0;
-let hours = 0;
+const secondsSelected = 10;
+const minutesSelected = 0;
+const hoursSelected = 0;
+let seconds = secondsSelected;
+let minutes = minutesSelected;
+let hours = hoursSelected;
+let launched = false;
+let working = true;
 let para = document.getElementById("timer");
 let button = document.getElementById('buttonLaunchReset');
+let workingIcon = document.getElementById('working');
+let chrono;
+
 printTime();
+
 button.addEventListener('click', () => {
-    let chrono = window.setInterval(addSecond, 1000);
-})
+    if (!launched){ //button is in "play" mode
+        chrono = window.setInterval(addSecond, 1000);
+        button.innerHTML = '<i class="fa-solid fa-arrow-rotate-left"></i>';
+        launched = true;
+    } else { //button is in "reset" mode
+        window.clearTimeout(chrono);
+        button.innerHTML = '<i class="fa-solid fa-play"></i>';
+        workingIcon.innerHTML = '<i class="fa-solid fa-person-digging"></i>';
+        seconds = secondsSelected;
+        minutes = minutesSelected;
+        hours = hoursSelected;
+        working = true;
+        launched = false;
+        printTime();
+    }
+});
+
+
 function addSecond() {
     if (seconds > 0){
         seconds--;
@@ -20,9 +45,26 @@ function addSecond() {
         }
     else{
         window.clearTimeout(chrono);
+        seconds = secondsSelected;
+        minutes = minutesSelected;
+        hours = hoursSelected;
+        working = !working;
+        if(working){ // end of pause time
+            seconds = secondsSelected;
+            minutes = minutesSelected;
+            hours = hoursSelected;
+            workingIcon.innerHTML = '<i class="fa-solid fa-person-digging"></i>';
+        } else { // end of working time
+            seconds = 0;
+            minutes = 5;
+            hours = 0;
+            workingIcon.innerHTML = '<i class="fa-solid fa-bed"></i>';
+        }
+        chrono = window.setInterval(addSecond, 1000);
     }
     printTime();
 }
+
 
 function printTime(){
     if (hours == 0){
